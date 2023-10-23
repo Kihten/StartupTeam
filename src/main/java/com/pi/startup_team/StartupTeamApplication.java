52e73ceae922a98007cc55b8a19627f9a51dc447
Yuriy Kikhtenko <kihtenkoura@gmail.com>
2023-10-23 20:08:41 +0600

    first

---
+++ src/main/java/com/pi/startup_team/StartupTeamApplication.java
@@ -0,0 +1,13 @@
+package com.pi.startup_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/startup_team/StartupTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.startup_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
