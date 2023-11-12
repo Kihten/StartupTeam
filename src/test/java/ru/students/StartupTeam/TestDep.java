43717849d8ac585d8db6cef78dd3e713ce03231f
Evgeniy Petrov <80803299+Evgguse@users.noreply.github.com>
2023-11-12 10:50:37 +0600

    Update TestDep.java

--- src/test/java/ru/students/StartupTeam/TestDep.java
+++ src/test/java/ru/students/StartupTeam/TestDep.java
@@ -1,5 +1,4 @@
 package ru.students.StartupTeam;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
