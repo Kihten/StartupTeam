093e8e39dc9a3e7b4ce722a6546d54f90fbabe6a
Evgeniy Petrov <80803299+Evgguse@users.noreply.github.com>
2023-11-12 10:46:10 +0600

    Dependency test created
    A test was created to check the functionality of the dependencies and driver

---
+++ src/test/java/ru/students/StartupTeam/TestDep.java
@@ -0,0 +1,30 @@
+package ru.students.StartupTeam;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class TestDep {
+    WebDriver driver = new EdgeDriver();
+
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void ft() {
+        driver.get ("https://ya.ru/");
+    }
+    @Test
+    public void st() {
+        driver.get ("https://yandex.ru/pogoda/omsk?utm_campaign=informer&utm_content=main_informer&utm_medium=web&utm_source=home&lat=54.989342&lon=73.368212");
+    }
+}
