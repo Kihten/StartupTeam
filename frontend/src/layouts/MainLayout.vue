c836b4fe4e50f9660f0c3c0b19a5fc3290c259c1
wwe_at <zvezda.20021@mail.ru>
2023-12-12 04:43:27 +0600

    changes main page

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -17,6 +17,7 @@
       <router-view />
     </q-page-container>
   </q-layout>
+
 </template>
 
 <script>
--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -33,26 +33,32 @@
           </div>
         </div>
       </div>
-      <q-input dense outlined square v-model="search" placeholder="Поиск"  
-        style="width: 450px;  margin-left: 450px; background: #b2b9b8"  />
-      <q-carousel
+      <q-input rounded outlined v-model="text" label="Поиск"
+        style="width: 450px;  margin-left: 450px; background: #cee5de; border-radius: 30px;">
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    
+      
+        <q-carousel
         v-model="slide"
         transition-prev="scale"
         transition-next="scale"
         swipeable
         animated
-        control-color="white"
+        control-color="black"
         navigation
         padding
         arrows
-        height="300px"
-        class="bg-primary text-white shadow-1 rounded-borders"
+        height="250px"
+        class="text-black"
       >
-        <q-carousel-slide :name="index" class="column no-wrap flex-center" v-for="(item, index) in projects" :key="index">
-          <q-icon name="style" size="56px" />
-          <div class="q-mt-md text-center col">
-            <p>{{ item.name }}</p>
-            <router-link :to="`/project/${item.id}`">Подробнее</router-link>
+        <q-carousel-slide style="background-color: #cee5de;" :name="index" class="no-wrap flex-center" v-for="(item, index) in projects" :key="index">
+          <div class="q-mt-md text-center col" >
+            <q-img :src="item.image" style="width:190px; height:190px;border-radius: 50%; left: -400px; margin-top: -10px;"></q-img>
+            <h6 style="margin-left: 300px; margin-top: -120px;">{{ item.description }} </h6>
+            <router-link style="margin-inline-start: 300px;" :to="`/project/${item.id}`">Подробнее</router-link>
           </div>
         </q-carousel-slide>
       </q-carousel>
@@ -64,6 +70,31 @@
       label-style="font-size: 1.1em"
     />
   </q-page>
+
+  <div style="columns: 4;  column-gap: 40px; margin-top: 40px; margin-left: 80px;">
+    <b>Контакты</b>
+    <p>644050, Российская федерация, город Омск, пр-т Мира д.11</p>
+    <p>+7 (3812) 65-34-07 </p>
+    <p>info@omgtu.ru</p>
+    <b>Приемная комиссия</b>
+    <p>+7 800-201-00-55</p>
+    <p>+7 (3812) 72-90-55</p>
+    <p>pk@omgtu.ru</p>
+    <p>priem@omgtu.ru</p>
+    <b>Управление внешних связей</b>
+    <p>+7 (3812) 62-32-57</p>
+    <p>+7 800 53-12-57</p>
+    <p>press_omgtu@mail.ru</p>
+    <p>644050</p>
+    <b>Техническая поддержка</b>
+    <p>webdev@omgtu.ru</p>
+    <p>+7 800 62-98-34</p>
+    <p>+7 (3812) 44-90-25</p>
+    <p>tecno_omgtu@mail.ru</p>
+
+  </div>
+
+
 </template>
  
 <script>
@@ -91,6 +122,11 @@ export default{
   },
   computed: {
     ...mapState(useCounterStore, ['projects']),
+  },
+  setup () {
+    return {
+      search: ref('')
+    }
   }
 }
 </script>
@@ -104,6 +140,5 @@ export default{
     font-size: 20px
     text-transform: uppercase
     font-weight: bolder
-  .column + .column
 
 </style>
